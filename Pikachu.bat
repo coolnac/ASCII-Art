0133e3d5160f2d3ef5bdb45c2e81aeb9279c8676
Nac Chou <coolnac@gmail.com>
2019-02-11 17:23:48 +0800

    Add files via upload

---
+++ Pikachu.bat
@@ -0,0 +1,53 @@
+@ECHO OFF
+ECHO.
+echo ¢i¢i¢iשß      ¢i¢i¢iשß  ¢i¢i¢i¢i¢iשß    ¢i¢i¢i¢i¢i¢iשß  ¢i¢iשß        ¢i¢iשß
+echo ¢i¢i¢i¢iשß  ¢i¢i¢i¢iשר¢i¢iשÝשששש¢i¢iשß¢i¢iשÝשששששששששו  ¢i¢iשר        ¢i¢iשר
+echo ¢i¢iשÝ¢i¢i¢i¢iשÝ¢i¢iשר¢i¢i¢i¢i¢i¢i¢iשר¢i¢iשר    ¢i¢i¢iשß¢i¢iשר  ¢iשß  ¢i¢iשר
+echo ¢i¢iשרשד¢i¢iשÝשו¢i¢iשר¢i¢iשÝשששש¢i¢iשר¢i¢iשר      ¢i¢iשר¢i¢iשר¢i¢i¢iשß¢i¢iשר
+echo ¢i¢iשר  שדשששו  ¢i¢iשר¢i¢iשר    ¢i¢iשרשד¢i¢i¢i¢i¢i¢iשÝשושד¢i¢i¢iשÝ¢i¢i¢iשÝשו
+echo שדשששו          שדשששושדשששו    שדשששו  שדשששששששששששו    שדשששששושדשששששו 
+echo.                        
+ECHO                                              ,-.
+ECHO                                           _.^|  ^'
+ECHO                                         .^'  ^| ^/
+ECHO                                       ,^'    ^|^'
+ECHO                                      ^/      ^/
+ECHO                        _..----^"^"---.^'      ^/
+ECHO  _.....---------...,-^"^"                  ,^'
+ECHO  `-._  \                                ^/
+ECHO      `-.+_            __           ,--. .
+ECHO           `-.._     .:  ).        (`--^"^| \
+ECHO                7    ^| `^" ^|         `...^'  \
+ECHO                ^|     `--^'     ^'+^"        ,^". ,^"^"-
+ECHO                ^|   _...        .____     ^| ^|^/    ^'
+ECHO           _.   ^|  .    `.  ^'--^"   ^/      `.^/     j
+ECHO          \^' `-.^|  ^'     ^|   `.   ^/        ^/     ^/
+ECHO          ^'     `-. `---^"      `-^"        ^/     ^/
+ECHO           \       `.                  _,^'     ^/
+ECHO            \        `                        .
+ECHO             \                                j
+ECHO              \                              ^/
+ECHO               `.                           .
+ECHO                 +                          \
+ECHO                 ^|                           L
+ECHO                 ^|                           ^|
+ECHO                 ^|  _ ^/,                     ^|
+ECHO                 ^| ^| L)^'..                   ^|
+ECHO                 ^| .    ^| `                  ^|
+ECHO                 ^'  \^'   L                   ^'
+ECHO                  \  \   ^|                  j
+ECHO                   `. `__^'                 ^/
+ECHO                 _,.--.---........__      ^/
+ECHO                ---.,^'---`         ^|   -j^"
+ECHO                 .-^'  ^'....__      L    ^|
+ECHO               ^"^"--..    _,-^'       \ l^|^|
+ECHO                   ,-^'  .....------. `^|^|^'
+ECHO                _,^'                ^/
+ECHO              ,^'                  ^/
+ECHO             ^'---------+-        ^/
+ECHO                      ^/         ^/
+ECHO                    .^'         ^/
+ECHO                  .^'          ^/
+ECHO                ,^'           ^/
+ECHO              _^'....----^"^"^"NAC
+TIMEOUT -1
